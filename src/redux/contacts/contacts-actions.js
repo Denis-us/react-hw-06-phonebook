927d08011add1c26038fbedd0e804962aee38e12
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-05-31 23:11:25 +0300

    Виправив дз

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,16 +1,7 @@
 import { createAction } from '@reduxjs/toolkit'
-import { v4 as uuidv4 } from 'uuid'
 
 
-const addContact = createAction('contacts/add', contact => {
-    return {
-        payload: {
-            id: uuidv4(),
-            name: contact.name,
-            number: contact.number
-        }
-    }
-})
+const addContact = createAction('contacts/add')
 const deleteContact = createAction('contacts/delete')
 const changeFilter = createAction('contacts/filter')
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,12 @@
-import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
-import {logger} from 'redux-logger'
+import {configureStore} from '@reduxjs/toolkit'
 import contactsReducer from '../redux/contacts/contacts-reducer'
 
-console.log(getDefaultMiddleware())
-
-const middleware = [...getDefaultMiddleware(), logger]
 
 const store = configureStore({
     reducer: {
         contacts: contactsReducer
     },
-    middleware,
-    devTools: process.env.NODE_ENV === 'development'
+
 })
 
 export default store
